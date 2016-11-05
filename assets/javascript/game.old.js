3a62c2ba600d6d9cd832cd1fa2a90b47d6be9627
JimmyJ <jimmyj.inbox@gmail.com>
2016-11-05 17:54:47 -0500

    recommit

---
+++ assets/javascript/game.old.js
@@ -0,0 +1,185 @@
+// HANGMAN JAVASCRIPT FILE //
+
+
+// NOTES TO SELF
+// 1. Need to go DRY re for loops written long ways
+
+//initially declaring global variables //
+//bank of words to use for game //
+var wordBank = ["rottweiler", "chihuahua", "labradoodle", "pug", "terrier", "pomeranian", "mutt", "dalmation", "husky", "boxer"];
+
+// bank of pictures to use in left box at start and after each win or loss//
+var picBank = ["rott.gif", "chi.gif", "labdl.gif", "pug2.gif", "jt.gif", "pomm.gif", "mutt.gif", "dal.gif", "husky.gif", "boxer.gif"];
+
+//bank of sounds to use when winning
+var soundBank = ["Rott.m4a", "Chi.m4a", "Lab.m4a", "Pug.m4a", "Terr.m4a", "Pomm.m4a", "Mutt.m4a", "Dal.m4a", "Husky.m4a", "Boxer.m4a"]
+
+//initiate variable total wins for player//
+var totalWins = 0;
+
+//initiate array for writing out initial blank spaces//
+var begBlank = [];
+
+//initial number of guesses left//
+var guessLeft = 15;
+
+//initiate array for letters picked so far by user//
+var lettersPicked = [];
+
+//picking of random word (multiple by total number of words)//
+var ranNum = Math.floor(Math.random()*10);
+var selectedWord = wordBank[ranNum];
+
+//user's current guess//
+var guess = "";
+
+//bad used letters//
+var badLetters = [];
+
+//good used letters//
+var goodLetters = [];
+
+//blanks spaces for game//
+var spacedOut = [];
+
+//counter to determine if word figured out//
+var wordFind = selectedWord.length;
+
+//counter to test if word solved//
+var numCount=0;
+
+//counter to determine if begBlank and spacedOut are equal
+var numRem = spacedOut.length
+
+//test sound
+var audio = "";
+//audio.play();
+
+//blank spacing//
+function letterSpaces () {
+
+	//writing out initial blank spaces//
+	begBlank = [];
+
+	//letters picked so far by user//
+	lettersPicked = [];
+
+	//picking of random word (multiple by total number of words)//
+	ranNum = Math.floor(Math.random()*10);
+	selectedWord = wordBank[ranNum];
+
+	//user's current guess//
+	guess = "";
+
+	//good used letters//
+	goodLetters = [];
+
+	//bad used letters//
+	badLetters = [];
+
+	//blanks spaces for game//
+	spacedOut = [];
+
+	//test to display random word to console...test successful //
+	console.log(selectedWord);
+	console.log("-----------");
+
+	//separate each letter of selected word into its own space in an arrary//
+	for (i=0; i<selectedWord.length; i++) {
+		spacedOut.push(selectedWord[i]);
+		begBlank.push(" _");
+	}
+
+	//test to display spaced out letters to console...test successful//
+	console.log(spacedOut);
+	console.log(spacedOut.length);
+
+	//counter to determine if begBlank and spacedOut are equal
+	numRem = spacedOut.length;
+
+	//populate initial page with initial data//
+	document.getElementById("headGame").innerHTML = "PRESS ANY KEY TO PLAY!";
+	document.getElementById("wordBlanks").innerHTML = begBlank.join(" ");
+	document.getElementById("winCount").innerHTML = totalWins;
+	document.getElementById("guessesLeft").innerHTML = guessLeft;
+	document.getElementById("usedLetters").innerHTML = badLetters;
+	document.getElementById("picLeft").innerHTML = "<img src='assets/images/letters.png'>";
+}
+
+//run function to space out letters of selected word//
+letterSpaces();
+
+//listen for keystrokes and then compare to word, populate word blanks, decrease guess counter, see if guess counter<1, see if won//
+document.onkeyup = function(event){
+
+	//assign player keystroke to guess variable
+	guess = String.fromCharCode(event.keyCode).toLowerCase();
+
+	//check if out of guesses and if need to reset word blanks
+	if (guessLeft <1) {
+		guessLeft = 16;
+		letterSpaces();
+	} else {};
+
+	//determine if guess is already accepted letter.  skip if so, continue if not.
+	if (guess == goodLetters[0] || guess == goodLetters[1] || guess == goodLetters[2] || guess == goodLetters[3] || guess == goodLetters[4] || guess == goodLetters[5] || guess == goodLetters[6] || guess == goodLetters[7] || guess == goodLetters[8]) {
+	} else {
+
+		//adding guessed letter to list of letters picked
+		lettersPicked.push(guess);
+
+		//test to see if random word and initial blank spacing work
+		console.log(guess);
+		console.log(spacedOut.length);
+		console.log(lettersPicked);
+		console.log(badLetters);
+
+		//begin loop to see if guessed letter matches selected word or not
+		for (i=0; i<spacedOut.length; i++){
+			if (guess==spacedOut[i]) {
+
+				//update word blanks
+				begBlank[i]=guess;
+				document.getElementById("wordBlanks").innerHTML = begBlank.join(" ");
+
+				//test if player figured out word
+				if (begBlank[i]==spacedOut[i]) {
+						numRem--
+						if (numRem<1) {
+							totalWins++;
+							document.getElementById("headGame").innerHTML = "CONGRATS! YOU GUESSED <BR><BR>" + selectedWord.toUpperCase() + "!<BR><BR>PRESS ANY KEY TO PLAY AGAIN";
+							document.getElementById("winCount").innerHTML = totalWins;
+							document.getElementById("picLeft").innerHTML = "<img src='assets/images/" + picBank[ranNum] + "'>";
+							audio = new Audio("assets/sounds/" + soundBank[ranNum]);
+							audio.play();
+							guessLeft = 0;
+						} else {};
+				} else {};
+
+				// add guessed letter to array of goodLetters
+				if (guess != goodLetters[0] && guess != goodLetters[1] && guess != goodLetters[2] && guess != goodLetters[3] && guess != goodLetters[4] && guess != goodLetters[5] && guess != goodLetters[6] && guess != goodLetters[7] && guess != goodLetters[8]) {
+					goodLetters.push(guess);				
+				} else {};
+			} else {};  
+		};
+			//actions for bad guess
+			if (guess != goodLetters[0] && guess != goodLetters[1] && guess != goodLetters[2] && guess != goodLetters[3] && guess != goodLetters[4] && guess != goodLetters[5] && guess != goodLetters[6] && guess != goodLetters[7] && guess != goodLetters[8] && guess != goodLetters[9]) {
+				
+				//display bad letters used and guesses left
+				if (guess != badLetters[0] && guess != badLetters[1] && guess != badLetters[2] && guess != badLetters[3] && guess != badLetters[4] && guess != badLetters[5] && guess != badLetters[6] && guess != badLetters[7] && guess != badLetters[8] && guess != badLetters[9] && guess != badLetters[10] && guess != badLetters[11] && guess != badLetters[12] && guess != badLetters[13] && guess != badLetters[14] && guess != badLetters[15] && guess != badLetters[16] && guess != badLetters[17] && guess != badLetters[18] && guess != badLetters[19] && guess != badLetters[20] && guess != badLetters[21] && guess != badLetters[22] && guess != badLetters[23] && guess != badLetters[24] && guess != badLetters[25]) {
+					guessLeft--;
+					badLetters.push(guess);
+					document.getElementById("usedLetters").innerHTML = badLetters;
+					document.getElementById("guessesLeft").innerHTML = guessLeft;
+				};
+
+				//determine if all guesses used and display lose screen
+				if (guessLeft<1) {
+					document.getElementById("headGame").innerHTML = "SORRY BUT YOU LOST.<BR><BR>  PRESS ANY KEY TO START AGAIN";
+					document.getElementById("picLeft").innerHTML = '<img src="assets/images/slap.gif">';
+				} else {};
+			};	
+		};
+
+	};
+
